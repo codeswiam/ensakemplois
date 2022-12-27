528095130e8e2176d1f08d479a88882b60ae61fe
manarika <115372119+manarika@users.noreply.github.com>
2022-12-27 18:59:42 +0100

    l

--- professeur.php
+++ professeur.php
@@ -1,19 +1,13 @@
 <html>
 <head>
-    <title>mycv</title>
+    <title>professeur</title>
     <link rel="stylesheet" href="style.css">
 </head>
-<style>
-    .pseudo{
-        color:red;
-        font-weight: bold;
-        text-decoration: underline;
-    }
-    input{
-        width: 500px;
-    }
-</style>
 <body>
+<header>
+    <a href="ajouterprof.php"> ajouter un professeur</a>
+</header>
+
 <table cellpadding="20px"cellspacing="10px" >
  <tr >
          <th >nom</th>
@@ -27,11 +21,18 @@ $sql1="SELECT * FROM prof" ;
 $result1=mysqli_query($link,$sql1);
 
  while($data1=mysqli_fetch_assoc ( $result1)){
-        echo"<tr>";
+        $sql2="SELECT nom,count(idseance) as nbseance FROM seance,profmod,prof where seance.idprofmod=profmod.idprofmod and prof.idprof=profmod.idprof and prof.idprof='".$data1['idprof']."'" ;
+    $result2=mysqli_query($link,$sql2);
+    $data2=mysqli_fetch_assoc ( $result2);
+     echo"<tr>";
         echo"<td>".$data1['nom']."</td> ";
         echo"<td>".$data1['prenom']."</td>";
+         echo"<td>". $data2['nbseance']*2 ."<nbsp> heures"."</td>";
         echo "</tr>";
     }
+
+
+
 ?>
 </table>
 </body>
