6830c512de0a5e3d4b126aaefef33eb8e5e18705
codeswiam <wiamcodes@gmail.com>
2023-01-10 10:29:46 +0100

    delete

--- connexion.php
+++
@@ -1,3 +0,0 @@
-<?php
-    $link = mysqli_connect("localhost", "root", "root", "emplois") or die('Echec de connexion.' . mysqli_error($link));
-?>
--- deconnexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    if (isset($_SESSION['prof']) or isset($_SESSION['admin'])){
-        $_SESSION = array(); // unsets all the session variables
-        session_destroy();
-    }
-    header("Location: acceuil.php");
-?>
